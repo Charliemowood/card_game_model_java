df495e8bf7a7c6455cda45e13eccbb02390bc48f
Charlie Wood <charliemowood@gmail.com>
2017-05-21 17:01:20 +0100

    Add second player

--- PlayerTest.java
+++ PlayerTest.java
@@ -3,14 +3,21 @@ import org.junit.*;
 
   public class PlayerTest {
     Player player;
+    Player second_player;
 
     @Before
     public void before() {
       player = new Player("Charlie");
+      second_player = new Player("Zuzana");
     }
 
     @Test
     public void hasName() {
       assertEquals("Charlie", player.getName());
+    }
+
+    @Test
+    public void hasNameSecondPlayer() {
+      assertEquals("Zuzana", second_player.getName());
     }
    }
